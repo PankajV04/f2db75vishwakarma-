9204c9db9169b7e1b3dd1b1ce144bd98ed747dee
S554775 <pankajvshwkrm8@gmail.com>
2022-11-29 22:40:28 -0600

    secured

--- models/search.js
+++ models/search.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const searchSchema = mongoose.Schema({ 
  color: String, 
  size: String, 
- price: Number 
+ price:{
+    type:Number,
+    min: 1000,
+    max: 8000 } 
 }) 
  
 module.exports = mongoose.model("Search", searchSchema) 
--- routes/search.js
+++ routes/search.js
@@ -11,7 +11,18 @@
 var express = require('express'); 
 const search_controlers= require('../controllers/search'); 
 var router = express.Router(); 
- 
+
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET searchs */ 
 router.get('/', search_controlers.search_view_all_Page ); 
 module.exports = router; 
@@ -20,11 +31,13 @@ module.exports = router;
 router.get('/detail', search_controlers.search_view_one_Page); 
 
 /* GET create costume page */ 
-router.get('/create', search_controlers.search_create_Page); 
+router.get('/create', secured, search_controlers.search_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', search_controlers.search_update_Page); 
+router.get('/update',  search_controlers.search_update_Page); 
  
 /* GET delete costume page */ 
-router.get('/delete', search_controlers.search_delete_Page); 
+router.get('/delete', secured, search_controlers.search_delete_Page); 
  
+/* GET update costume page */ 
+router.get('/update', secured, search_controlers.search_update_Page); 
